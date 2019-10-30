1622bd2f6e8ba678ef4fdf4ca84d9116a03ab9b6
GoodGameGabe <41083088+GoodGameGabe@users.noreply.github.com>
2019-10-30 19:15:08 -0400

    Update gabe.py

--- teams/gabe.py
+++ teams/gabe.py
@@ -1,12 +1,13 @@
 
 from pyparrot.Minidrone import Mambo
+import os
 
 # you will need to change this to the address of YOUR mambo
-mamboAddr = "e0:14:d0:63:3d:d0"
+mamboAddr =  os.environ.get('DRONE_ADDRESS')
 
 # make my mambo object
 # remember to set True/False for the wifi depending on if you are using the wifi or the BLE to connect
-mambo = Mambo(mamboAddr, use_wifi=True)
+mambo = Mambo(mamboAddr, use_wifi=False)
 
 print("trying to connect")
 success = mambo.connect(num_retries=3)
